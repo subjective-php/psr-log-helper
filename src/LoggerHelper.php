37f6d4463df573a2683dfe3c65e2e0781d3cce00
chadicus <chadwickgray@gmail.com>
2016-10-18 13:14:11 -0400

    Fix PHPCS issues

--- src/LoggerHelper.php
+++ src/LoggerHelper.php
@@ -4,23 +4,26 @@ namespace Chadicus\Psr\Log;
 
 use Psr\Log\InvalidArgumentException;
 
+/**
+ * Abstract utiltity class for assisting with PSR logger tasks.
+ */
 abstract class LoggerHelper
 {
-	/**
+    /**
      * Helper method to ensure a given level is one of the eight known RFC 5424 levels.
-	 *
+     *
      * @param string $level The level to validate.
      *
      * @return void
      *
-     * @throws InvalidArgumentException Throw if $level is not a know RFC-5424 level
+     * @throws InvalidArgumentException Throw if $level is not a know RFC-5424 level.
      */
     final public static function validateLevel($level)
     {
-		if (!is_string($level) || !defined('\\Psr\\Log\\LogLevel::' . strtoupper($level))) {
+        if (!is_string($level) || !defined('\\Psr\\Log\\LogLevel::' . strtoupper($level))) {
             throw new InvalidArgumentException('Given $level was not a known LogLevel');
         }
-	}
+    }
 
     /**
      * Interpolates context values into the message placeholders.
@@ -32,18 +35,18 @@ abstract class LoggerHelper
      */
     final public static function interpolateMessage($message, array $context)
     {
-		$context = array_filter(
-			$context,
-			function ($value) {
-				return (is_scalar($value) || (is_object($value) && method_exists($value, '__toString')));
-			}
-		);
+        $context = array_filter(
+            $context,
+            function ($value) {
+                return (is_scalar($value) || (is_object($value) && method_exists($value, '__toString')));
+            }
+        );
 
-		$search = [];
-		$replace = [];
+        $search = [];
+        $replace = [];
         foreach ($context as $key => $value) {
-			$search[] = "{{$key}}";
-			$replace[] = (string)$value;
+            $search[] = "{{$key}}";
+            $replace[] = (string)$value;
         }
 
         return str_replace($search, $replace, $message);
