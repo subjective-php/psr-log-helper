1a982087c4b75583279230fb1bd8e718aec79658
chadicus <chadwickgray@gmail.com>
2017-04-21 22:15:48 -0400

    Add MessageValidatorTrait

---
+++ src/MessageValidatorTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Chadicus\Psr\Log;
+
+use Psr\Log\InvalidArgumentException;
+
+/**
+ * Trait for validating PSR-3 log messages.
+ */
+trait MessageValidatorTrait
+{
+    /**
+     * Determines if the specified message is legal under PSR-3.
+     *
+     * @param mixed $message The message to validate.
+     *
+     * @return void
+     *
+     * @throws InvalidArgumentException Throw if $message is not a know RFC-5424 message.
+     */
+    protected function validateMessage($message)
+    {
+        if (is_scalar($message)) {
+            return;
+        }
+
+        if (is_object($message) && method_exists($message, '__toString')) {
+            return;
+        }
+
+        throw new InvalidArgumentException('Given $message was a valid string value');
+    }
+}
---
+++ tests/MessageValidatorTraitTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ChadicusTest\Psr\Log;
+
+use Chadicus\Psr\Log\MessageValidatorTrait;
+
+/**
+ * @coversDefaultClass \Chadicus\Psr\Log\MessageValidatorTrait
+ * @covers ::<private>
+ */
+final class MessageValidatorTraitTest extends \PHPUnit_Framework_TestCase
+{
+    use MessageValidatorTrait;
+
+    /**
+     * @param mixed $message The message that will validate.
+     *
+     * @test
+     * @covers ::validateMessage
+     * @dataProvider provideValidValidateData
+     *
+     * @return void
+     */
+    public function validateMessageValid($message)
+    {
+        $this->assertNull($this->validateMessage($message));
+    }
+
+    /**
+     * Provides valid messages for testing.
+     *
+     * @return array
+     */
+    public function provideValidValidateData()
+    {
+        return [
+            ['a string' => 'the message'],
+            ['a boolean' => true],
+            ['an int' => 123],
+            ['a float' => 12.3],
+            ['an object' => new \SplFileInfo(__FILE__)],
+        ];
+    }
+
+    /**
+     * @test
+     * @covers ::validateMessage
+     * @expectedException \Psr\Log\InvalidArgumentException
+     *
+     * @return void
+     */
+    public function validateMessageInvalid()
+    {
+        $this->validateMessage(new \StdClass());
+    }
+}
